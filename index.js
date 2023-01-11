f0be53e423f83b95886e9d06acad9b9694156eaa
karthik0145 <karthikshriram5111@gmail.com>
2023-01-12 00:25:42 +0530

    feat: add functionality to check prime

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function add(a, b){
     return a+b;
 }
+function isPrime(a)
+{
+    var i;
+    for(i=2;i<Math.sqrt(a);i++)
+    {
+        if(a%i==0)
+        return false;
+    }
+    return true;
+    
+}
