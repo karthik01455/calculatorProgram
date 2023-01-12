79e07bd4c08a2e7c47e8a49fee1baed7caea6f33
karthik0145 <karthikshriram5111@gmail.com>
2023-01-12 22:03:31 +0530

    feat: add functionlity for checking prime

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ function sub(a, b) {
 function div(a, b) {
   return a / b;
 }
-
+function isPrime(a)
+{
+    var i;
+    i=0;
+    for(i=2;i<Math.sqrt(a);i++)
+    {
+        if(a%i==0)
+        return false;
+    }
+    return true;
+}
 function isEven(a) {
   if (a % 2 == 0) {
     return true;
