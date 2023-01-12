2a129bcd90837e2e537aa43d219777c2619c5f8d
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-12 23:49:01 +0530

    feat: add functionalites of add sub mul div on clicking buttons

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Features ----
+
 function add(a, b) {
   return a + b;
 }
@@ -10,10 +12,49 @@ function div(a, b) {
   return a / b;
 }
 
-function isEven(a) {
-  if (a % 2 == 0) {
-    return true;
-  } else {
-    return false;
-  }
+function mul(a, b) {
+  return a * b;
 }
+
+// Calculation ----
+
+let a = document.querySelector("#input-a");
+let b = document.querySelector("#input-b");
+
+// console.log(a.value);
+// console.log(b.value);
+
+addbtn.addEventListener("click", () => {
+  let inta = parseInt(a.value);
+  let intb = parseInt(b.value);
+  text.innerHTML = `<h2>${add(inta, intb)}</h2>`;
+  console.log(add(inta, intb));
+});
+
+subbtn.addEventListener("click", () => {
+  let inta = parseInt(a.value);
+  let intb = parseInt(b.value);
+  text.innerHTML = `<h2>${sub(inta, intb)}</h2>`;
+  console.log(sub(inta, intb));
+});
+
+mulbtn.addEventListener("click", () => {
+  let inta = parseInt(a.value);
+  let intb = parseInt(b.value);
+  text.innerHTML = `<h2>${mul(inta, intb)}</h2>`;
+  console.log(mul(inta, intb));
+});
+
+divbtn.addEventListener("click", () => {
+  let inta = parseInt(a.value);
+  let intb = parseInt(b.value);
+  text.innerHTML = `<h2>${div(inta, intb)}</h2>`;
+  console.log(div(inta, intb));
+});
+
+// calc.addEventListener("click", () => {
+//   console.log(a.value);
+//   console.log(b.value);
+// });
+
+// console.log("Samim");
